4db3706ca73d576d52afd5d7144970365e85fb04
Jake Meyer <jakewmeyer@gmail.com>
2022-04-26 20:03:22 -0500

    refactor status code error handling

--- src/routes.rs
+++ src/routes.rs
@@ -1,13 +1,16 @@
-use actix_web::{get, web::Path, HttpResponse, Responder};
+use actix_web::{get, HttpRequest, HttpResponse, Responder};
 use http::StatusCode;
 
 #[get("/{status_code}")]
-pub async fn get_status_code(path: Path<u16>) -> impl Responder {
-    let status_code = StatusCode::from_u16(path.into_inner());
-    match status_code {
-        Ok(status_code) => {
+pub async fn get_status_code(req: HttpRequest) -> impl Responder {
+    if let Some(status_code) = req.match_info().get("status_code") {
+        let status_code = status_code.parse::<StatusCode>();
+        if let Ok(status_code) = status_code {
             HttpResponse::build(status_code).body(status_code.canonical_reason().unwrap_or(""))
+        } else {
+            HttpResponse::NotFound().body("Invalid status code")
         }
-        Err(_) => HttpResponse::NotFound().body("Invalid status code"),
+    } else {
+        HttpResponse::NotFound().body("Invalid status code")
     }
 }
